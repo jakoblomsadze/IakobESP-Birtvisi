e89d4b422ce25f472485855c0ae6b28f93e0059a
jakoblomsadze <67922164+jakoblomsadze@users.noreply.github.com>
2025-08-09 19:54:17 +0400

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,6 @@ dependencies {
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
     implementation("com.google.android.material:material:1.12.0")
-    debugImplementation("androidx.compose.ui:ui-tooling")
+debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 }
